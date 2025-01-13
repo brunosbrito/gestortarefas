ff3bc0b41ea73f22d0056114d2cb61c261629eef
brunosbrito <brunobrito728@gmail.com>
2025-01-13 11:09:00 -0300

    atualizado interface service order

--- src/interfaces/ServiceOrderInterface.ts
+++ src/interfaces/ServiceOrderInterface.ts
@@ -1,10 +1,10 @@
 export interface ServiceOrder {
     id: string;
-    project: string;
+    projectId: number;
     serviceOrderNumber: string;
     description: string;
     status: string;
+    notes: string;
     createdAt: string;
     updatedAt: string; 
   }
-  
